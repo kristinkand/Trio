8edb7f417a7a378fdf57aea0d7d7af56ee8a4db6
kristinkand <163904051+kristinkand@users.noreply.github.com>
2026-02-19 12:52:28 +0100

    Refactor deleteGlucose function into NightscoutAPI
    deleted the previous entry and added it back in under "func deleteGlucose"

--- Trio/Sources/Services/Network/Nightscout/NightscoutAPI.swift
+++ Trio/Sources/Services/Network/Nightscout/NightscoutAPI.swift
@@ -219,7 +219,39 @@ extension NightscoutAPI {
 
         debugPrint("Delete successful for ID \(id)")
     }
+    
+    func deleteGlucose(withId id: String) async throws {
+        var components = URLComponents()
+        components.scheme = url.scheme
+        components.host = url.host
+        components.port = url.port
+        components.path = "/api/v1/entries/"
+        components.queryItems = [
+            URLQueryItem(name: "find[_id][$eq]", value: id)
+        ]
 
+        guard let url = components.url else {
+            throw URLError(.badURL)
+        }
+
+        var request = URLRequest(url: url)
+        request.allowsConstrainedNetworkAccess = false
+        request.timeoutInterval = Config.timeout
+        request.httpMethod = "DELETE"
+
+        if let secret = secret {
+            request.addValue(secret.sha1(), forHTTPHeaderField: "api-secret")
+        }
+
+        let (_, response) = try await URLSession.shared.data(for: request)
+
+        guard let httpResponse = response as? HTTPURLResponse, (200 ... 299).contains(httpResponse.statusCode) else {
+            throw URLError(.badServerResponse)
+        }
+
+        debugPrint("Delete glucose from NS successful for entry with ID \(id)")
+    }
+    
     func deleteInsulin(withId id: String) async throws {
         var components = URLComponents()
         components.scheme = url.scheme
@@ -573,34 +605,3 @@ private extension String {
         return hexBytes.joined()
     }
 }
-func deleteGlucose(withId id: String) async throws {
-        var components = URLComponents()
-        components.scheme = url.scheme
-        components.host = url.host
-        components.port = url.port
-        components.path = "/api/v1/entries/"
-        components.queryItems = [
-            URLQueryItem(name: "find[_id][$eq]", value: id)
-        ]
-
-        guard let url = components.url else {
-            throw URLError(.badURL)
-        }
-
-        var request = URLRequest(url: url)
-        request.allowsConstrainedNetworkAccess = false
-        request.timeoutInterval = Config.timeout
-        request.httpMethod = "DELETE"
-
-        if let secret = secret {
-            request.addValue(secret.sha1(), forHTTPHeaderField: "api-secret")
-        }
-
-        let (_, response) = try await URLSession.shared.data(for: request)
-
-        guard let httpResponse = response as? HTTPURLResponse, (200 ... 299).contains(httpResponse.statusCode) else {
-            throw URLError(.badServerResponse)
-        }
-
-        debugPrint("Delete glucose from NS successful for entry with ID \(id)")
-    }
