3e72095a6b1ec858b2c56d6fcb9d35f0269d9e56
kristinkand <kristin.kandschur@proton.me>
2026-08-02 18:38:04 +0200

    Update HomeStateModel.swift

--- Trio/Sources/Modules/Home/HomeStateModel.swift
+++ Trio/Sources/Modules/Home/HomeStateModel.swift
@@ -680,6 +680,7 @@ extension Home {
             displayYgridLines = settingsManager.settings.yGridLines
             bolusDisplayThreshold = settingsManager.settings.bolusDisplayThreshold
             thresholdLines = settingsManager.settings.rulerMarks
+            showPreviousDayGlucose = settingsManager.settings.showPreviousDayGlucose
             showCarbsRequiredBadge = settingsManager.settings.showCarbsRequiredBadge
             enableQuickPickTreatments = settingsManager.settings.enableQuickPickTreatments
             forecastDisplayType = settingsManager.settings.forecastDisplayType
